7e52bc60131d7689186205419b8e907d1e2d2f52
EvgSuslov <94070733+EvgSuslov@users.noreply.github.com>
2022-06-13 22:29:29 +0300

    Update lab1.py
    eng fix

--- lab1.py
+++ lab1.py
@@ -1,28 +1,36 @@
-#метод шифровки перестановка
+'''  
+the permutation cipher
+    task - random shuffle + code of shuffle
+        working as like 
+    in input random string ex. ('secret place')
+    if simple - than make another string
+                        .add in v.2  - if simple + (' ' at the end)
+    return ciphered sting  + [21365]
+'''
 import sys
 import random
 
-min_n=3 #Минимальное число для алгоритма деления 
-k=0 #для определения TRUE FALSE на простое
-print("введите сообщение")
+min_n=3 #minimal sep. for string
+k=0 #mor simple of not
+print("input string: \n") 
 s = input()
-print('кол-во букв в тексте' + str(len(s))) #считает символы
+print('len of string is: ' + str(len(s))) 
 len_ar=(len(s))
-for i in range(2, len_ar // 2+1): #проверяет простое ли число и останваливает скрипт если да
+for i in range(2, len_ar // 2+1): #if simple - than do anoter
     if len_ar % i == 0:
         k+=1
 if (k!=0):
     print(str(len_ar)+' это простое число')
 else:
     print(str(len_ar) + ' это не простое число'+ '\n' +'Введите иное сообщение')
-    sys.exit()                                   #тут в строке надо добавить лишний символ в конце но я 
-                                                    #прерываниемамаамама
-while len_ar % min_n != 0:                       # while true пока остаот от деленеия не равен 0
-    if len_ar % min_n == 0:                      # если остаток от деления равен 0 
+    sys.exit()                                    
+                                                    
+while len_ar % min_n != 0:                       
+    if len_ar % min_n == 0:                      
         print ('') 
         break
     elif len_ar % min_n != 0: 
-         min_n+=1                          # если остаток от деления не равен 0
+         min_n+=1                          
 else:  
     rng = list(range(0, min_n))
     
